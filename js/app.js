f0692eff8c2063e2de6e8ff2fc6548434f41e187
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-07-25 14:09:10 -0700

    update before the branch

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// Approaching defining the problem domain: Break things down:
+
+// 1. Global variables
+
+
+// 2. Executable code
+
+
+// 3. Constructor
+
+
+// 4. DOM References
+
